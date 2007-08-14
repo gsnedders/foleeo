50ea7ad9172cb773dc1e0036406463f5fecd6f08
Geoffrey Sneddon <geoffers@gmail.com>
2007-08-14 14:24:51 +0100

    Move month and date lists to Base_Parse_Date

--- date.php
+++ date.php
@@ -4,6 +4,50 @@ abstract class Base_Parse_Date
 {
 	protected $date;
 	
+	protected $day = array(
+		'mon' => 1,
+		'monday' => 1,
+		'tue' => 2,
+		'tuesday' => 2,
+		'wed' => 3,
+		'wednesday' => 3,
+		'thu' => 4,
+		'thursday' => 4,
+		'fri' => 5,
+		'friday' => 5,
+		'sat' => 6,
+		'saturday' => 6,
+		'sun' => 7,
+		'sunday' => 7,
+	);
+	
+	protected $month = array(
+		'jan' => 1,
+		'january' => 1,
+		'feb' => 2,
+		'february' => 2,
+		'mar' => 3,
+		'march' => 3,
+		'apr' => 4,
+		'april' => 4,
+		'may' => 5,
+		// No long form of May
+		'jun' => 6,
+		'june' => 6,
+		'jul' => 7,
+		'july' => 7,
+		'aug' => 8,
+		'august' => 8,
+		'sep' => 9,
+		'september' => 8,
+		'oct' => 10,
+		'october' => 10,
+		'nov' => 11,
+		'november' => 11,
+		'dec' => 12,
+		'december' => 12,
+	);
+	
 	public function __construct($date)
 	{
 		$this->date = $date;
@@ -48,16 +92,14 @@ abstract class Base_Parse_Date
 
 class Parse_Date extends Base_Parse_Date
 {
-	private $short_mon = array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
-	
 	protected function asctime()
 	{
 		static $pcre;
 		if (!$pcre)
 		{
 			$space = '[\x09\x20]+';
-			$wday_name = '(Mon|Tue|Wed|Thu|Fri|Sat|Sun)';
-			$mon_name = '(' . implode($this->short_mon, '|') . ')';
+			$wday_name = '(' . implode(array_keys($this->day), '|') . ')';
+			$mon_name = '(' . implode(array_keys($this->month), '|') . ')';
 			$day = '([0-2][0-9]|3[01])';
 			$hour = '([01][0-9]|2[0-3])';
 			$min = '([0-5][0-9])';
@@ -67,8 +109,8 @@ class Parse_Date extends Base_Parse_Date
 		}
 		if (preg_match($pcre, $this->date, $match))
 		{
-			$mon = array_search($match[2], $this->short_mon) + 1;
-			return gmmktime($match[4], $match[5], $match[6], $mon, $match[3], $match[7]);
+			$month = $this->month[strtolower($match[2])];
+			return gmmktime($match[4], $match[5], $match[6], $month, $match[3], $match[7]);
 		}
 		else
 		{
@@ -76,5 +118,8 @@ class Parse_Date extends Base_Parse_Date
 		}
 	}
 }
+
+$parser = new Parse_Date('Sun Sep 16 01:03:52 1973');
+var_dump($parser->parse());
 
 ?>

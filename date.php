8533b0636ced27b91234586c915aa244f9c887fe
Geoffrey Sneddon <geoffers@gmail.com>
2007-08-14 17:18:50 +0100

    Add RFC2822 dates

--- date.php
+++ date.php
@@ -68,7 +68,7 @@ abstract class Base_Parse_Date
 		
 		foreach ($methods as $method)
 		{
-			if (($returned = call_user_func(array(&$this, $method))) !== false)
+			if (stripos($method, 'date_') === 0 && ($returned = call_user_func(array(&$this, $method))) !== false)
 			{
 				return $returned;
 			}
@@ -92,7 +92,7 @@ abstract class Base_Parse_Date
 
 class Parse_Date extends Base_Parse_Date
 {
-	protected function asctime()
+	protected function date_asctime()
 	{
 		static $pcre;
 		if (!$pcre)
@@ -117,9 +117,112 @@ class Parse_Date extends Base_Parse_Date
 			return false;
 		}
 	}
+	
+	private function remove_rfc2822_comments($string)
+	{
+		$string = (string) $string;
+		$position = 0;
+		$length = strlen($string);
+		$depth = 0;
+		
+		$output = '';
+		
+		while ($position < $length && ($pos = strpos($string, '(', $position)) !== false)
+		{
+			$output .= substr($string, $position, $pos - $position);
+			$position = $pos + 1;
+			if ($string[$pos - 1] !== '\\')
+			{
+				$depth++;
+				while ($depth && $position < $length)
+				{
+					$position += strcspn($string, '()', $position);
+					if ($string[$position - 1] === '\\')
+					{
+						$position++;
+						continue;
+					}
+					elseif (isset($string[$position]))
+					{
+						switch ($string[$position])
+						{
+							case '(':
+								$depth++;
+								break;
+							
+							case ')':
+								$depth--;
+								break;
+						}
+						$position++;
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+			else
+			{
+				$output .= '(';
+			}
+		}
+		$output .= substr($string, $position);
+		
+		return $output;
+	}
+	
+	protected function date_rfc2822()
+	{
+		static $pcre;
+		if (!$pcre)
+		{
+			$wsp = '[\x09\x20]';
+			$fws = '(?:' . $wsp . '+|' . $wsp . '*(?:\x0D\x0A' . $wsp . '+)+)';
+			$optional_fws = $fws . '?';
+			$day_name = '(' . implode(array_keys($this->day), '|') . ')';
+			$month = '(' . implode(array_keys($this->month), '|') . ')';
+			$day = '([0-9]{1,2})';
+			$hour = $minute = $second = '([0-9]{2})';
+			$year = '([0-9]{2,4})';
+			$num_zone = '([+\-])([0-9]{2})([0-9]{2})';
+			$character_zone = '([A-Z]{1,5})';
+			$zone = '(' . $num_zone . '|' . $character_zone . ')';
+			$pcre = '/(?:' . $optional_fws . $day_name . $optional_fws . ',)?' . $optional_fws . $day . $fws . $month . $fws . $year . $fws . $hour . $optional_fws . ':' . $optional_fws . $minute . '(?:' . $optional_fws . ':' . $optional_fws . $second . ')?' . $fws . $zone . '/i';
+		}
+		if (preg_match($pcre, $this->remove_rfc2822_comments($this->date), $match))
+		{
+			static $timezones = array('EST' => -18000, 'EDT' => -14400, 'CST' => -21600, 'CDT' => -18000, 'MST' => -25200, 'MDT' => -21600, 'PST' => -28800, 'PDT' => -25200);
+			$month = $this->month[strtolower($match[3])];
+			
+			// Numeric timezone
+			if ($match[9] !== '')
+			{
+				$timezone = $match[10] * 3600;
+				$timezone += $match[11] * 60;
+				if ($match[9] === '-')
+				{
+					$timezone = 0 - $timezone;
+				}
+			}
+			// Character timezone
+			elseif (isset($timezones[strtoupper($match[12])]))
+			{
+				$timezone = $timezones[strtoupper($match[12])];
+			}
+			// Assume everything else to be -0000
+			else
+			{
+				$timezone = 0;
+			}
+			
+			return gmmktime($match[5], $match[6], $match[7], $month, $match[2], $match[4]) - $timezone;
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
-
-$parser = new Parse_Date('Sun Sep 16 01:03:52 1973');
-var_dump($parser->parse());
 
 ?>
